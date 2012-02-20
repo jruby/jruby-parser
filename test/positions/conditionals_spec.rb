69d2c95b02a7e5e664a6faa072f08ae8ef20b225
Thomas E. Enebo <tom.enebo@gmail.com>
2012-02-20 16:39:24 -0600

    Add beginnings of conditional expectations

---
+++ test/positions/conditionals_spec.rb
@@ -0,0 +1,15 @@
+$LOAD_PATH.unshift File.dirname(__FILE__) + "/../helpers"
+require 'java'
+require File.dirname(__FILE__) + "/../../dist/JRubyParser.jar"
+require 'parser_helpers'
+require 'node_helpers'
+
+describe Parser do
+  [1.8, 1.9].each do |v|
+    it "should parse an unless on alias [#{v}]" do
+      ast = parse("alias p ** unless method_defined? :p", v)
+      ast.find(:alias).tap { |a| a.should have_position(0, 0, 0, 10) }
+      ast.find(:if).tap { |i| i.should have_position(0, 0, 0, 36) }
+    end
+  end
+end
