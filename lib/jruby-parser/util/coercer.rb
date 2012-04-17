eab95f9a446cdd6eff8e4465370b3e89e1f57a50
Thomas E. Enebo <tom.enebo@gmail.com>
2012-04-17 14:40:07 -0500

    Use do end instead of string form of class_eval

--- lib/jruby-parser/util/coercer.rb
+++ lib/jruby-parser/util/coercer.rb
@@ -1,27 +1,27 @@
 module JRubyParser
   module Receiver
     def self.included(cls)
-      cls.class_eval <<-EOF
+      cls.class_eval do
         def receiver=(value)
            value = value.to_ast_node if value.respond_to? :to_ast_node
            old_value = getReceiver
            value.position = old_value.position unless value.position    
            setReceiver(value)
         end
-      EOF
+      end
     end
   end
   
   module Value
     def self.included(cls)
-      cls.class_eval <<-EOF
+      cls.class_eval do
         def value=(value)
            value = value.to_ast_node if value.respond_to? :to_ast_node
            old_value = getValue
            value.position = old_value.position unless value.position    
            setValue(value)
         end
-      EOF
+      end
     end
   end  
 end
