58df3f53fdc948207aab04eda7151ce544cd2510
Thomas E. Enebo <tom.enebo@gmail.com>
2013-03-12 13:58:02 -0500

    Lexer initialization in netbeans leads to uncommon scenario where lex_state has not been set yet.  Add guard

--- src/org/jrubyparser/lexer/Lexer.java
+++ src/org/jrubyparser/lexer/Lexer.java
@@ -629,6 +629,14 @@ public class Lexer {
     }
 
     private void determineExpressionState() {
+        // Some way netbeans is using the lexer ends up causing a scenario where lex_state is
+        // not properly initialized.  EXPR_BEG is the logical starting point so I will just
+        // set it if null;
+        if (lex_state == null) {
+            setState(LexState.EXPR_BEG);
+            return;
+        }
+        
         switch (lex_state) {
         case EXPR_FNAME: case EXPR_DOT:
             setState(LexState.EXPR_ARG);
