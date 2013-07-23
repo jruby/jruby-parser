b841e8b6fa8a22a368731cf446c0cefad6261c9e
Eric West <esw9999@gmail.com>
2013-07-23 10:11:01 -0500

    Moves NodeDiff, creates SequenceMatcher

---
+++ src/org/jrubyparser/util/diff/NodeDiff.java
@@ -0,0 +1,12 @@
+package org.jrubyparser.util.diff;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: eric
+ * Date: 7/23/13
+ * Time: 7:06 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class NodeDiff
+{
+}
