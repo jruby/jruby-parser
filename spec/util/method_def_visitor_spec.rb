731edb7ef534822ba3380a08fa52f16b59839561
Thomas E. Enebo <tom.enebo@gmail.com>
2013-11-20 13:58:04 -0600

    Add some more specs for new method_defs method

--- spec/util/method_def_visitor_spec.rb
+++ spec/util/method_def_visitor_spec.rb
@@ -2,34 +2,45 @@ require_relative '../helpers'
 
 import org.jrubyparser.util.MethodDefVisitor
 
+def modules_in(code)
+  find_modules(parse(code))
+end
+
 def find_modules(node)
-  ret_nodes = []
-  node.child_nodes.each do |n|
-    if n.kind_of? org.jrubyparser.ast.IModuleScope
-      ret_nodes << n
-    end
-    ret_nodes = ret_nodes.concat find_modules(n)
+  node.child_nodes.inject([]) do |modules, n|
+    modules << n if n.kind_of? org.jrubyparser.ast.IModuleScope
+    modules.concat find_modules(n)
   end
-  ret_nodes if ret_nodes
 end
 
 describe MethodDefVisitor do
-  it 'should find a method defined on a class' do
-    root = parse("class Example\ndef hello\nend\nend")
-    klasses = find_modules(root)
-    klasses[0].method_defs[0].name.should =~ /hello/
+  it 'should find no methods defined in a class' do
+    modules_in("class Example\nend").first.method_defs.should == []
+  end
+
+  it 'should find a method defined in a class' do
+    modz = modules_in("class Example\ndef hello\nend\nend")
+    modz.first.method_defs.map(&:name).should =~ %w{hello}
+  end
+
+  it 'should find methods defined in a class' do
+    modz = modules_in("class Example\ndef hello\nend;def world; end\nend")
+    modz.first.method_defs.map(&:name).should =~ %w{hello world}
   end
 
   it 'should not find a method defined on an inner class' do
-    root = parse("module Ex\nclass Example\ndef hello\nend\nend\nend")
-    modz = find_modules(root)
-    modz[0].method_defs.size.should == 0
+    modz = modules_in("module Ex\nclass Example\ndef hello\nend\nend\nend")
+    modz.first.method_defs.size.should == 0
   end
 
   it 'should not find a method defined inside a method' do
-    root = parse("class Example\ndef hello\ndef hello_too\nend\nend\nend")
-    modz = find_modules(root)
-    modz[0].method_defs.size.should == 1
+    modz = modules_in("class Example\ndef hello\ndef hello_too\nend\nend\nend")
+    modz.first.method_defs.size.should == 1
+  end
+
+  it 'should find a method defined in block scope in module body' do
+    modz = modules_in("module Ex\nloop do\ndef hello\nend\nend\nend")
+    modz.first.method_defs.size.should == 1
   end
 end
 
