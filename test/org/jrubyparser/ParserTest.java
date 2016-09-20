3055edf9c8130860298c2a432ee0119b323e12b8
Jason Yeo <jasonyeo88@gmail.com>
2016-09-20 14:55:32 +0800

    Remove unused imports

--- test/org/jrubyparser/ParserTest.java
+++ test/org/jrubyparser/ParserTest.java
@@ -6,14 +6,13 @@ package org.jrubyparser;
 
 import org.jrubyparser.ast.LambdaNode;
 import org.jrubyparser.ast.Node;
-import org.jrubyparser.lexer.ReaderLexerSource;
 import org.jrubyparser.parser.ParserConfiguration;
+import org.junit.Test;
 
 import java.io.StringReader;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -23,7 +22,7 @@ public class ParserTest {
 
   ////////////////////////////////// Methods \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
-  @org.junit.Test
+  @Test
   public void testArrowSyntax() throws Exception {
     // https://github.com/jruby/jruby-parser/issues/36
     final List<String> ruby = Arrays.asList("-> (message) { puts message }");
