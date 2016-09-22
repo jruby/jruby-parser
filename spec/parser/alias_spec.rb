925db4c85c342e4e003155dc26f20cec97d0de20
Jason Yeo <jasonyeo88@gmail.com>
2016-09-22 13:42:02 +0800

    Fix typo

--- spec/parser/alias_spec.rb
+++ spec/parser/alias_spec.rb
@@ -1,11 +1,11 @@
 require_relative '../helpers'
 
 describe Parser do
-  it "should parse alias with quotationm arks" do
+  it "should parse alias with quotation marks" do
     parse(%Q{alias :'<==>' :"foo"}).find_node(:alias).tap do |aliasn|
       expect(aliasn.new_name.find_node(:symbol).name).to eq '<==>'
       expect(aliasn.old_name.find_node(:symbol).name).to eq 'foo'
     end
   end
-end
 
+end
