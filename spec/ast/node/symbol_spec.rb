513618745f5e94aeba2fa05d279c0bdd11123798
Jason Yeo <jasonyeo88@gmail.com>
2016-09-26 16:44:50 +0800

    Add spec for SymbolNode#name

--- spec/ast/node/symbol_spec.rb
+++ spec/ast/node/symbol_spec.rb
@@ -1,10 +1,19 @@
 require 'helpers'
 
 describe org::jrubyparser.ast.SymbolNode do
+  describe '#lexical_name' do
+    it 'returns the lexical name of the symbol' do
+      parse(':foo').find_node(:symbol).tap do |sym|
+        expect(sym.lexical_name).to eq ':foo'
+      end
+    end
+  end
 
-  it 'returns the lexical name of the symbol' do
-    parse(':foo').find_node(:symbol).tap do |sym|
-      expect(sym.lexical_name).to eq ':foo'
+  describe '#name' do
+    it 'returns the name of the symbol' do
+      parse(':foo').find_node(:symbol).tap do |sym|
+        expect(sym.name).to eq 'foo'
+      end
     end
   end
 end
