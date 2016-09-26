07fac73f1b87f1cecc6a0d150f2c44e2e66af0b0
Jason Yeo <jasonyeo88@gmail.com>
2016-09-26 17:18:29 +0800

    Add spec for InstVarNode#name

--- spec/ast/node/inst_var_spec.rb
+++ spec/ast/node/inst_var_spec.rb
@@ -2,9 +2,17 @@ require 'helpers'
 
 describe org::jrubyparser::ast::InstVarNode do
   describe '#lexical_name' do
-    it 'returns the lexical name fo the instance variable' do
+    it 'returns the lexical name of the instance variable' do
       parse('@foo').find_node(:instvar).tap do |ivar|
         expect(ivar.lexical_name).to eq '@foo'
+      end
+    end
+  end
+
+  describe '#name' do
+    it 'returns the name of the instance variable' do
+      parse('@foo').find_node(:instvar).tap do |ivar|
+        expect(ivar.name).to eq '@foo'
       end
     end
   end
